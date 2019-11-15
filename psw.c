5efb3eb3b1643ddd6c45b3f4df6c4a09b9071013
Spyros Mouchlianitis <56831313+SpyrosMouchlianitis@users.noreply.github.com>
2019-11-15 13:36:37 +0200

    Update psw.c

--- psw.c
+++ psw.c
@@ -3,24 +3,30 @@
 It has at least 6 characters and at most 20 characters.
 It must contain at least one lowercase letter, at least one uppercase letter, and at least one digit.
 It must NOT contain three repeating characters in a row ("...aaa..." is weak, but "...aa...a..." is strong, assuming other conditions are met).
-Write a function strongPasswordChecker(s), that takes a string s as input, and return the MINIMUM change required to make s a strong password. If s is already strong, return 0.
-*/
+Write a function strongPasswordChecker(s), that takes a string s as input, and return the MINIMUM change required to make s a strong password. If s is already strong, return 0.*/
+
 
 #include <stdio.h>
 #include <ctype.h>
 
 int main(){
-    int errors=0;
+    int errors;
+    int charcount=0;
+    int upper=0;
+    int lower=0;
+    int digits=0;
+    int triple=0;
+    int i;
     char psw[20];
     printf("Enter a password\n");
     scanf("%s", psw);
     
+    
     do
     {
-    
+        errors=0;
+        charcount=0;
         //Checks the password's length
-        int charcount = 0;
-        int i;
         for (i = 0; psw[i]; i++)
         {
             charcount += 1;
@@ -28,15 +34,15 @@ int main(){
         if (charcount<6)
         {
             printf("Password must be longer than 6 characters\n");
+            errors++;
         }else if (charcount>20)
         {
             printf("Password must be no longer than 20 characters\n");
+            errors++;
         }
 
 
         //Checks if the password has one lower case and one upper case character
-        int upper=0;
-        int lower=0;
         for ( i = 0; psw[i]!='\0'; i++)
         {
             if (psw[i]>='A' && psw[i]<='Z')
@@ -53,15 +59,16 @@ int main(){
         if (lower==0)
         {
             printf("The password needs at least 1 lower case character\n");
+            errors++;
         }
         if (upper==0)
         {
             printf("The password needs at least 1 upper case character\n");
+            errors++;
         }
         
 
         //Checks if the password has at least one digit
-        int digits=0;
         for ( i = 0; psw[i]!='\0'; i++)
         {
             if (psw[i]>='0' && psw[i]<='9')
@@ -72,11 +79,12 @@ int main(){
         if (digits==0)
         {
             printf("The password needs at least 1 digit\n");
+            errors++;
         }
         
 
         //Checks if the password has three same letters in a row e.x("...aaa...")
-        int triple=0;
+        triple=0;
         for ( i = 0; psw[i]!='\0'; i++)
         {
             if (psw[i]==psw[i+1] && psw[i+1]==psw[i+2])
@@ -88,7 +96,14 @@ int main(){
         
         if (triple!=0)
         {
-            printf("Do not use the same characters in a row e.x(...aaa...)");
+            printf("Do not use the same characters in a row e.x(...aaa...)\n");
+            errors++;
+        }
+
+
+        if(errors!=0){
+            printf("Try a different password\n");
+            scanf("%s", psw);
         }
     } while (errors!=0);
     
